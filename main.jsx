5fdc7f760359c4567776386754aab139fc2f322d
UniversalDummy <davyhawk0109@gmail.com>
2026-06-16 20:37:32 -0400

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import './style.css'
+import './responsive.css'
 import TasksContainer from './tasksContainer.jsx'
 createRoot(document.getElementById('root')).render(
     <TasksContainer />
